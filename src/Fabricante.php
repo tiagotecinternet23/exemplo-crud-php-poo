c0d978ad66d50f3819f6d2ad556ad29a08a2af4b
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-10-03 11:51:48 -0300

    Método construct e chamada do conecta

--- src/Fabricante.php
+++ src/Fabricante.php
@@ -1,12 +1,22 @@
 <?php
 namespace ExemploCrudPoo;
+use PDO;
 final class Fabricante {
 
     private int $id;
     private string $nome;
-
-
     
+    /* Esta propriedade receberá os recursos
+    PDO através da classe Banco (dependência deste projeto) */
+    private PDO $conexao;
+
+    public function __construct() {
+        /* No momento em que um objeto Fabricante for
+        criado, automaticamente será feita a chamada
+        do método "conecta" existente na classe Banco. */
+        $this->conexao = Banco::conecta();
+    }
+
     public function getId(): int {
         return $this->id;
     }
