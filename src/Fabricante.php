3fdd6654fd442b8a41e9ebfcf0daf2eda9c0b193
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-10-05 09:20:44 -0300

    Método lerUmFabricante

--- src/Fabricante.php
+++ src/Fabricante.php
@@ -44,6 +44,20 @@ final class Fabricante {
         }
     }
 
+    public function lerUmFabricante():array {
+        $sql = "SELECT * FROM fabricantes WHERE id = :id";
+    
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+            $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro ao carregar: ".$erro->getMessage());
+        }
+    
+        return $resultado;
+    } 
 
 
 
--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -4,20 +4,7 @@ require_once "conecta.php";
 
 
 
-function lerUmFabricante(PDO $conexao, int $idFabricante):array {
-    $sql = "SELECT * FROM fabricantes WHERE id = :id";
 
-    try {
-        $consulta = $conexao->prepare($sql);
-        $consulta->bindValue(":id", $idFabricante, PDO::PARAM_INT);
-        $consulta->execute();
-        $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
-    } catch (Exception $erro) {
-        die("Erro ao carregar: ".$erro->getMessage());
-    }
-
-    return $resultado;
-} 
 
 
 function atualizarFabricante(PDO $conexao, string $nome, int $id):void {
